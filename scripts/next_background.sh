7d4d77802b32f67f15c96cd4cc9ef928cdd3b717
Black Cat <ktekuchev@gmail.com>
2019-11-25 20:16:39 +0200

    [Fix] temporarily disabled random wallpapers changing

--- scripts/next_background.sh
+++ scripts/next_background.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-while (true) 
-do
+if [[ $3 == "continous" ]]; then
+
+    while (true) 
+    do
+        echo $(exec ~/.config/awesome/scripts/random_file.sh $1)
+        sleep $2s;
+    done
+else
     echo $(exec ~/.config/awesome/scripts/random_file.sh $1)
-    sleep $2s;
-done
+fi
